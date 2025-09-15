6c8af0fe3aa4985c71bb330a3129ab88ad47bc2c
Akah Harmony <harmonyakah12@gmail.com>
2025-09-15 03:25:29 +0100

    Update Analysis & Visualization.R

--- Analysis & Visualization.R
+++ Analysis & Visualization.R
@@ -1,1 +1,6 @@
-
+# Plotting using ggplot2() function
+# Weekly Usage by Member Type
+ggplot(data = Cleaned_Divvy2021) +
+  geom_bar(mapping = aes(x = Day_of_week, fill = member_casual), position = "dodge") +
+  labs(title = "Weekly Usage by Member Type") +
+  scale_y_continuous(labels = label_number(accuracy = 1))
