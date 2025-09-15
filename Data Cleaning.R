e4cdc400e519382270319d04d4bc53050b0368e4
Akah Harmony <harmonyakah12@gmail.com>
2025-09-15 01:20:05 +0100

    Update Data Cleaning.R

--- Data Cleaning.R
+++ Data Cleaning.R
@@ -1,1 +1,5 @@
+# Cleaning and removing spaces, parentheses, camelCases, etc.
+Merged_Divvy2021 <- clean_names(Merged_Divvy2021)
 
+# Removing empty rows and columns, leave the c() empty, selects both the columns and rows
+remove_empty(Merged_Divvy2021, which = c())
