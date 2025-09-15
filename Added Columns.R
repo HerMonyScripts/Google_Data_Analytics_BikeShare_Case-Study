8a2699cd3c44f5565ea6674a0e91755169907e51
Akah Harmony <harmonyakah12@gmail.com>
2025-09-15 01:40:20 +0100

    Update Added Columns.R

--- Added Columns.R
+++ Added Columns.R
@@ -1,1 +1,8 @@
-
+# Adding needed columns for analysis
+Merged_Divvy2021 <- Merged_Divvy2021 %>%
+  mutate(
+    Day_of_week = wday(started_at, label = TRUE),
+    Start_hour = format(as.POSIXct(started_at)),
+    Month = format(as.Date(started_at)),
+    Trip_duration = difftime(ended_at, started_at, units = "mins")
+  )
