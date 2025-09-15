710e0ddb750edb91a1476cb3938921393e7110f9
Akah Harmony <harmonyakah12@gmail.com>
2025-09-15 04:06:11 +0100

    Update Analysis & Visualization.R

--- Analysis & Visualization.R
+++ Analysis & Visualization.R
@@ -4,3 +4,10 @@ ggplot(data = Cleaned_Divvy2021) +
   geom_bar(mapping = aes(x = Day_of_week, fill = member_casual), position = "dodge") +
   labs(title = "Weekly Usage by Member Type") +
   scale_y_continuous(labels = label_number(accuracy = 1))
+
+# Hourly Usage Throughout the Week
+ggplot(data = Cleaned_Divvy2021) +
+  geom_bar(mapping = aes(x = Start_hour, fill = member_casual)) +
+  facet_wrap(~Day_of_week) +
+  labs(title = "Hourly Usage Throughout the Week") +
+  scale_y_continuous(labels = label_number(accuracy = 1))
