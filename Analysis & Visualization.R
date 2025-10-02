63205742d0167f31a64ccd5a26b83771851d0daf
Akah Harmony <harmonyakah12@gmail.com>
2025-10-02 06:13:30 +0100

    Update Analysis & Visualization.R

--- Analysis & Visualization.R
+++ Analysis & Visualization.R
@@ -1,3 +1,31 @@
+# Descriptive Analysis
+# Number of rides per day of the week
+Cleaned_df %>%
+  group_by(member_casual, Day_of_week) %>%
+  summarise(number_of_rides = n()) %>%
+  arrange(member_casual, Day_of_week)
+
+# Number of rides by bike type
+Cleaned_df %>%
+  group_by(member_casual, rideable_type) %>%
+  summarise(number_of_rides = n())
+
+# Top 10 most popular start stations for casual riders
+Cleaned_df %>%
+  filter(member_casual == "casual") %>%
+  group_by(start_station_name) %>%
+  summarise(number_of_starts = n()) %>%
+  arrange(desc(number_of_starts)) %>%
+  head(10)
+
+# Top 10 most popular start stations for member riders
+Cleaned_df %>%
+  filter(member_casual == "member") %>%
+  group_by(start_station_name) %>%
+  summarise(number_of_starts = n()) %>%
+  arrange(desc(number_of_starts)) %>%
+  head(10)
+
 # Plotting using ggplot2() function
 # Weekly Usage by Member Type
 ggplot(data = Cleaned_Divvy2021) +
