d65c3a2f695471fde0c103b4fa9cd6850cc19dad
Akah Harmony <harmonyakah12@gmail.com>
2025-10-02 16:33:03 +0100

    Update Added Columns.R

--- Added Columns.R
+++ Added Columns.R
@@ -2,7 +2,7 @@
 Merged_Divvy2021 <- Merged_Divvy2021 %>%
   mutate(
     Day_of_week = wday(started_at, label = TRUE),
-    Start_hour = format(as.POSIXct(started_at)),
-    Month = format(as.Date(started_at)),
-    Trip_duration = difftime(ended_at, started_at, units = "mins")
+    Start_hour = hour(started_at),
+    Month = month(started_at, label = TRUE),
+    Trip_duration = as.numeric(difftime(ended_at, started_at, units = "mins"))
   )
