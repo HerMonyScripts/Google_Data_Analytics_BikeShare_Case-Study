acf103904e56c27b1bee3e11eb9224e998a71aa1
Akah Harmony <harmonyakah12@gmail.com>
2025-10-02 02:32:39 +0100

    Update Data Cleaning.R

--- Data Cleaning.R
+++ Data Cleaning.R
@@ -1,5 +1,9 @@
 # Cleaning and removing spaces, parentheses, camelCases, etc.
 Merged_Divvy2021 <- clean_names(Merged_Divvy2021)
 
-# Removing empty rows and columns, leave the c() empty, selects both the columns and rows
-remove_empty(Merged_Divvy2021, which = c())
+# Remove trips less than 1 minutes or longer than 24 hours (1440 mins)
+Cleaned_Divvy2021 <- Merged_Divvy2021 %>%
+  filter(Trip_duration > 1 & Trip_duration < 1440)
+
+# A check of the cleaned data
+summary(Cleaned_Divvy2021$Trip_duration)
